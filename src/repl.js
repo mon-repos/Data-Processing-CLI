438e7f4884d6e538a060d84956dbee7ca3516aab
mon-repos <180408776+mon-repos@users.noreply.github.com>
2026-03-16 00:43:42 +0100

    Fix minor issues

--- src/repl.js
+++ src/repl.js
@@ -22,7 +22,7 @@ export function repl(state) {
           state,
         );
       } catch (error) {
-        console.log("Operation failed (command operation)", error);
+        console.log("Operation failed (command operation)");
       }
     }
     rl.prompt();
@@ -33,51 +33,3 @@ export function repl(state) {
     process.exit(0);
   });
 }
-
-/*function parseLine(line) {
-  let lineArguments = line.trim().split(" ");
-  return lineArguments;
-}
-
-/*
-  // 1. A callback function
-  const checkLine = (usersAnswer) => {
-    let trimmedAnswer = usersAnswer.trim(); // 1.1 trimming a line
-    switch (
-      trimmedAnswer // 1.2 make a switch for actions in case of various line values
-    ) {
-      case "uptime":
-        console.log("Uptime: " + process.uptime().toFixed(2) + "s");
-        rl.prompt();
-        break;
-      case "cwd":
-        console.log(process.cwd());
-        rl.prompt();
-        break;
-      case "date":
-        let curDate = new Date().toISOString();
-        console.log(curDate);
-        rl.prompt();
-        break;
-      case "exit":
-        rl.close();
-        break;
-      default:
-        console.log("Unknown command");
-        rl.prompt();
-    }
-  };
-
-  // 2. Events
-  rl.on("line", (input) => checkLine(input));
-  rl.on("SIGINT", () => rl.close());
-  rl.on("close", () => {
-    console.log("Goodbye!");
-    process.exit(0);
-  });
-
-  // 3. prompt
-  //console.log("Commands: uptime, cwd, date, exit, ctrl+c");
-  rl.prompt();
-}
-  */
